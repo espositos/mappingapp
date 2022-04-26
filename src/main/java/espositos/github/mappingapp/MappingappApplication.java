fcda224ba10ffffe03910cef6b91b442ed1c9b6a
Hamish <hamish.martin@gmail.com>
2022-04-26 20:38:36 +0200

    Adding basic spring config

---
+++ src/main/java/espositos/github/mappingapp/MappingappApplication.java
@@ -0,0 +1,13 @@
+package espositos.github.mappingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MappingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MappingappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/espositos/github/mappingapp/MappingappApplicationTests.java
@@ -0,0 +1,13 @@
+package espositos.github.mappingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MappingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
